c8045b7972f7f9dced3988fe147f99244c743228
Neha Goswami <Neha.Goswami@in.jrdsi.com>
2017-12-21 15:29:04 +0530

    First commit.

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('geoLocationApp')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout, toastr) {
+    var vm = this;
+
+
+
+ 
+
+   
+  }
+})();
