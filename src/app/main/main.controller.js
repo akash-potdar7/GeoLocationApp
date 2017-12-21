734808c06e8c2f296a8be30beaa3601b253cc711
Neha Goswami <Neha.Goswami@in.jrdsi.com>
2017-12-21 17:23:09 +0530

    form validation done for login

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,9 +6,25 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, toastr) {
+  function MainController($timeout, toastr, $mdToast, $rootScope,$state) {
     var vm = this;
+    vm.user={};
 
+      vm.login=function()
+        {
+        if (!vm.user.email || !vm.user.password) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .content("Fields cant be empty")
+                    .position("top right")
+                    .theme("error-toast")
+                    .hideDelay(3000)
+            );
+        }
+
+       
+
+    }
 
 
  
